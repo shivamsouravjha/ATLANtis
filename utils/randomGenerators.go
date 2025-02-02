6ed247e68636f3ec79eeee208ddfd15fda518c6e
Shivam Sourav Jha <60891544+shivamsouravjha@users.noreply.github.com>
2025-02-02 06:38:12 +0530

    Update randomGenerators.go

--- utils/randomGenerators.go
+++ utils/randomGenerators.go
@@ -2,10 +2,11 @@ package utils
 
 import (
 	"strings"
-
+	"fmt"
 	"github.com/google/uuid"
 )
 
 func GeneratorUUID(strLen int) string {
+	fmt.Println("apple")
 	return strings.Replace(uuid.NewString(), "-", "", -1)[:strLen]
 }
